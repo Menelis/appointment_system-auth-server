c3edb2cf1231d6b4978495eb81fc9eaded2b76c9
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-19 01:52:16 +0200

    Persists Oath2 clients to db

--- src/main/java/co/appointment/DatabaseInitializer.java
+++ src/main/java/co/appointment/DatabaseInitializer.java
@@ -1,15 +1,28 @@
 package co.appointment;
 
+import co.appointment.config.AppConfigProperties;
 import co.appointment.entity.Role;
 import co.appointment.repository.RoleRepository;
 import co.appointment.shared.constant.RoleConstants;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.Set;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -17,11 +30,27 @@ import java.util.Optional;
 public class DatabaseInitializer implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
+    private final RegisteredClientRepository registeredClientRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AppConfigProperties appConfigProperties;
 
 
     @Override
     public void run(String... args) {
         seedRoles();
+        seedRegisteredClients();
+    }
+    private void seedRegisteredClients() {
+        List<RegisteredClient> registeredClients = appConfigProperties.getRegisteredClients()
+                .stream()
+                .map(this::mapToRegisteredClient)
+                .toList();
+        registeredClients.forEach(registeredClient -> {
+            RegisteredClient optionalRegisteredClient = registeredClientRepository.findByClientId(registeredClient.getClientId());
+            if(optionalRegisteredClient == null) {
+                registeredClientRepository.save(registeredClient);
+            }
+        });
     }
     private void seedRoles() {
         Set<String> roles = Set.of(
@@ -34,5 +63,43 @@ public class DatabaseInitializer implements CommandLineRunner {
                 roleRepository.save(new Role(role));
             }
         });
+    }
+    private RegisteredClient mapToRegisteredClient(final AppConfigProperties.RegisteredClientSetting client) {
+        RegisteredClient.Builder registeredClient = RegisteredClient
+                .withId(UUID.randomUUID().toString());
+        if(!StringUtils.hasText(client.getClientId())) {
+            throw new IllegalArgumentException("Client Id is required");
+        }
+        if(client.getClientAuthenticationMethods().isEmpty()) {
+            throw new IllegalArgumentException("At least one client authentication method is required");
+        }
+        if(StringUtils.hasText(client.getClientSecret())) {
+            registeredClient.clientSecret(passwordEncoder.encode(client.getClientSecret()));
+        }
+        registeredClient.clientId(client.getClientId())
+                .authorizationGrantTypes(authorizationGrantTypes -> authorizationGrantTypes
+                        .addAll(client.getAuthorizationGrantTypes()
+                                .stream()
+                                .map(AuthorizationGrantType::new)
+                                .collect(Collectors.toSet())))
+                .clientAuthenticationMethods(clientAuthenticationMethods -> clientAuthenticationMethods
+                        .addAll(client.getClientAuthenticationMethods()
+                                .stream()
+                                .map(ClientAuthenticationMethod::new)
+                                .collect(Collectors.toSet())))
+                .redirectUris(redirectUris -> redirectUris
+                        .addAll(client.getRedirectUris()))
+                .postLogoutRedirectUris(postLogOutRedirectUris -> postLogOutRedirectUris
+                        .addAll(client.getPostLogoutRedirectUris()))
+                .scopes(scopes -> scopes
+                        .addAll(client.getScopes()))
+                .clientSettings(ClientSettings.builder()
+                        .requireProofKey(client.getClientSettings().isRequireProofOfKey())
+                        .build())
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenTimeToLive(Duration.ofHours(client.getTokenSetting().getAccessTokenTimeToLive()))
+                        .refreshTokenTimeToLive(Duration.ofHours(client.getTokenSetting().getRefreshTokenTimeToLive()))
+                        .build());
+        return registeredClient.build();
     }
 }
