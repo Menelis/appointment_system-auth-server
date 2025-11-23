db0234a516075288633af650df45173aaf135489
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-24 01:30:16 +0200

    Disabled unrecognized fields

--- src/main/java/co/appointment/mixin/UserDetailsImplMixin.java
+++ src/main/java/co/appointment/mixin/UserDetailsImplMixin.java
@@ -1,6 +1,7 @@
 package co.appointment.mixin;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.security.core.GrantedAuthority;
 
@@ -18,4 +19,19 @@ public abstract class UserDetailsImplMixin {
             @JsonProperty("contactNo") final String contactNo,
             @JsonProperty("password") final String password,
             @JsonProperty("authorities") final Collection<? extends GrantedAuthority> authorities) {}
+
+    @JsonIgnore
+    public abstract String getUsername();
+
+    @JsonIgnore
+    public abstract boolean isEnabled();
+
+    @JsonIgnore
+    public abstract boolean isAccountNonExpired();
+
+    @JsonIgnore
+    public abstract boolean isAccountNonLocked();
+
+    @JsonIgnore
+    public abstract boolean isCredentialsNonExpired();
 }
